649b9266f39ba90a6cb208d33ba437f0e7c13d4f
clauderam <info.cloudram@gmail.com>
2026-09-13 13:53:19 +0300

    swift fix 2

--- submodules/TelegramUI/Components/Gifts/GiftViewScreen/Sources/GiftViewScreen.swift
+++ submodules/TelegramUI/Components/Gifts/GiftViewScreen/Sources/GiftViewScreen.swift
@@ -2503,7 +2503,7 @@ private final class GiftViewSheetContent: CombinedComponent {
         
         let giftCompositionExternalState = GiftCompositionComponent.ExternalState()
         
-        return { (context: ViewUpdateContext) in
+        return { context -> CGSize in
             let environment = context.environment[ViewControllerComponentContainer.Environment.self].value
             
             let component = context.component
